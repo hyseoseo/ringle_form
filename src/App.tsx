6897ee2a98fa780ca57a1a59aad59cb0dae97d48
hyseoseo <heeyoungseo83@gmail.com>
2021-10-05 12:08:39 +0900

    chore: cra + antd setting

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import { Button } from 'antd';
+import logo from './logo.svg';
+import './App.css';
+
+const App: FC = () => {
+  return (
+    <div className='App'>
+      <Button type='primary'>Add</Button>
+    </div>
+  );
+};
+
+export default App;
